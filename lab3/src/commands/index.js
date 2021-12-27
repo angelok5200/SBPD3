142053aabdd80d9611162f6e743950b9d8ce592c
angelok5200 <61203677+angelok5200@users.noreply.github.com>
2021-12-28 00:19:47 +0200

    Add files via upload

---
+++ lab3/src/commands/index.js
@@ -0,0 +1,17 @@
+const exit = require('./exit');
+const cd = require('./cd');
+const mkdir = require('./mkdir');
+const vi = require('./vi');
+const pwd = require('./pwd');
+const cuser = require('./cuser');
+const crights = require('./crights');
+
+module.exports = {
+    exit,
+    cd,
+    mkdir,
+    vi,
+    pwd,
+    cuser,
+    crights,
+}
